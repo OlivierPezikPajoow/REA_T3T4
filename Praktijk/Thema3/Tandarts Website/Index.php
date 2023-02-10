f5a72cb8c985649cd3004c47e21e05c10231c93e
Olivier Pezik Pajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-02-10 09:20:25 +0100

    Website Tandarts Praktijkopdracht

---
+++ Praktijk/Thema3/Tandarts Website/Index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+    Auteur: Olivier Pezik Pajoow
+    Aanmaakdatum: <10-02-2023>
+
+    Omschrijving:
+
+-->
+<html>
+<head>
+    <title>
+    TANDARTS DEN BOSCH
+    </title>
+    <!--    declaratie tekenset-->
+    <meta charset="UTF-8">
+    <!--    declaratie viewport-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--    link naar het CSS bestand-->
+    <link rel="stylesheet" href="/Praktijk/Thema3/Tandarts%20Website/styles/style.css">
+    <!--    link naar het script-->
+    <script src="" defer></script>
+</head>
+<body>
+    <header>
+        <img id="HomeLogo" src="/Praktijk/Thema3/Tandarts%20Website/Images/TandartsLogo.png" alt="Tandarts wim">
+        <nav>
+            <ol>
+                <li>
+
+                </li>
+            </ol>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
