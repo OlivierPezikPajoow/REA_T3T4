f035fe137415bb01d48093d90670e43964275e2c
Olivier Pezik Pajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-02-12 15:54:35 +0100

    CSS opdracht 2.1

--- Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
+++ Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
@@ -53,10 +53,16 @@
         <!-- Plaats je code / uitwerking hieronder -->
         <p>
             <?php
-            // Voorbeeldcode
-            echo "Op deze plek komt de uitwerking an de oefening te staan";
             echo "PHP is een serverside programmeer taal, dit houd in dat alles op de webserver gedaan wordt, zo zie je op jouw client niks
-            terug van wat er op de server is geschreven. Zo kun je via PHP een database oproepen en deze in leesbaar HTML weergeven."
+            terug van wat er op de server is geschreven. Zo kun je via PHP een database oproepen en deze in leesbaar HTML weergeven.";
+
+            echo "<h1>Git & Github</h1>";
+            echo "Github is een plek waar programmeurs hun programma's kunnen opslaan en delen, via GIT worden hun programma's geupload naar hun
+            account. Telkens als ze iets veranderen aan hun code kunnen ze hun code committen, zo wordt de code opgeslagen, als je je code naarna
+            pusht dan wordt het geupload naar je git en kan je het vervolgens zien in je repository.";
+            /*
+             * echo zorgt ervoor dat wat je in een PHP onderdeel zet alsnog zichbaar is op de webpagina, via HTML.
+             */
             ?>
         </p>
     </section>
