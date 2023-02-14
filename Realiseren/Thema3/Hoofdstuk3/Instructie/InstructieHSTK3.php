889a276a071787eb31d5d6f01cefa886c5b44ecf
Olivier Pezik Pajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-02-14 11:01:38 +0100

    instructie hoofdstuk 3

---
+++ Realiseren/Thema3/Hoofdstuk3/Instructie/InstructieHSTK3.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: Olivier Pezik Pajoow
+ * Date: 14-2-2023
+ * File: InstructieHSTK3php
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="styles/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+
+        </header>
+
+        <main id="wrapper">
+
+        </main>
+    </body>
+</html>
+
