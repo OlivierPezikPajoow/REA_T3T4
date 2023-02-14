1d72d3a79d6043785fbabd733d85bc5fa1564cb6
Olivier Pezik Pajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-02-14 11:34:48 +0100

    Hoofdstuk 2

--- Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
+++ Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
@@ -37,10 +37,10 @@
             </li>
             <ul>
                 <li>
-                    <a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php">Oefening 2.1</a>
+                    <a href="/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 2.1</a>
                 </li>
                 <li>
-                    <a href="/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening2.2.php">Oefening 2.2</a>
+                    <a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php">Oefening 2.2</a>
                 </li>
             </ul>
         </ul>
