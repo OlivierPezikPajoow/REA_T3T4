8cae20be0576e20aab7d4288bc8fc738b53c8186
OlivierPezikPajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-02-15 11:34:37 +0100

    Instructie Include

---
+++ Realiseren/Thema3/Hoofdstuk3/Include/NAV.php
@@ -0,0 +1,8 @@
+<nav>
+    <h2>Hoofdmenu</h2>
+    <h3>Hoofdstuk 1:</h3>
+    <ul>
+        <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 1.1</a></li>
+        <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.2.php">Oefening 1.2</a></li>
+    </ul>
+</nav>
---
+++ Realiseren/Thema3/Hoofdstuk3/Instructie/Instructie_Include.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * User: Olivier Pezik Pajoow
+ * Date: Date
+ * File: T3_REA_Oefening1.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'Realiseren instructies thema 3 en 4.';
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>
+        <?php
+        echo 'Instructies';
+        ?>
+    </h1>
+</header>
+<?php
+//Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+?>
+<main id="wrapper">
+    <?php
+    include "../Include/NAV.php"
+    ?>
+    <section>
+        <h2>
+            Uitwerkingen.
+        </h2>
+
+        <!-- Plaats je code / uitwerking hieronder -->
+        <p>
+            <?php
+            // Voorbeeldcode
+            echo "Hello World!";
+            ?>
+        </p>
+    </section>
+</main>
+</body>
+</html>
