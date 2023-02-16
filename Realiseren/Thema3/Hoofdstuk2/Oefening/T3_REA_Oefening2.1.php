edb37461f1e85c94f4ea3f5580522159888d56e0
OlivierPezikPajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-02-16 09:35:56 +0100

    T3 Rea 3.1

--- Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
+++ Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
@@ -35,22 +35,26 @@
             <li>
                 <h3>Hoofdstuk 2:</h3>
             </li>
-            <ul>
-                <li>
-                    <a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php">Oefening 2.1</a>
-                </li>
-                <li>
-                    <a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.2.php">Oefening 2.2</a>
-                </li>
-            </ul>
+            <li>
+                <ul>
+                    <li>
+                        <a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php">Oefening 2.1</a>
+                    </li>
+                    <li>
+                        <a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.2.php">Oefening 2.2</a>
+                    </li>
+                </ul>
+            </li>
             <li>
                 <h3>Hoofdstuk 3:</h3>
             </li>
-            <ul>
-                <li>
-                    <a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/T3_REA_Oefening_3_1.php">Oefening 3.1</a>
-                </li>
-            </ul>
+            <li>
+                <ul>
+                    <li>
+                        <a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/T3_REA_Oefening_3_1.php">Oefening 3.1</a>
+                    </li>
+                </ul>
+            </li>
         </ul>
     </nav>
     <section>
