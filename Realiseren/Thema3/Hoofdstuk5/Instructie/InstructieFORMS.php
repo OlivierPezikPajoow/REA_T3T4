d40615d8568aa9cb129b1ce8dcd4f6371ec3a717
OlivierPezikPajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-03-07 12:40:07 +0100

    Uitleg forms

---
+++ Realiseren/Thema3/Hoofdstuk5/Instructie/InstructieFORMS.php
@@ -0,0 +1,8 @@
+<form action="Results.php" method="post">
+    <label>Voornaam:</label>
+    <input type="text" name="firstname">
+    <label>Hoeveel uren skip je per week</label>
+    <label><input type="radio" name="uren" value="Minder dan 5"> Minder dan 5</label>
+    <label><input type="radio" name="uren" value="Meer dan 5">Meer dan 5</label>
+    <input type="submit">
+</form>
---
+++ Realiseren/Thema3/Hoofdstuk5/Instructie/Results.php
@@ -0,0 +1,5 @@
+<?php
+$firstname = $_POST['firstname'];
+$uren = $_POST['uren'];
+echo  $firstname;
+echo $uren;
