3764257aa75b4801dbe0989ce560a4ce85cd92d5
OlivierPezikPajoow <olivier.pezikpajoow@edu-kw1c.nl>
2023-03-10 11:42:26 +0100

    Praktijkopdracth tandarts

--- Praktijk/Thema3/ProjectTandarts/Pages/Results.php
+++ Praktijk/Thema3/ProjectTandarts/Pages/Results.php
@@ -1,2 +1,10 @@
 <?php
 $brush = $_POST['brush'];
+$cleanser = $_POST['cleanser'];
+$plaque = $_POST['plaque'];
+$electric = $_POST['electric'];
+$fluoride = $_POST['fluoride'];
+$breath = $_POST['breath'];
+$scraper = $_POST['scraper'];
+$blood = $_POST['blood'];
+$minutes = $_POST['minutes'];
