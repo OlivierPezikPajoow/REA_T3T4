d30b268c1cfe76eba30c61dbddbfa085a3ff507d
Administrator <olivier.pezikpajoow@edu-kw1c.nl>
2023-09-11 13:49:12 +0200

    Stageopdracht

--- Stage/StageOpdracht/Index.php
+++ Stage/StageOpdracht/Index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 <header>
-    <img id="OlivierLogo" src="Images/TempLogo.png" alt="Logo Naam">
+    <img id="OlivierLogo" src="Images/LogoName.png" alt="Logo Naam">
     <nav>
         <ul>
             <li>
@@ -49,7 +49,7 @@
             Hello and welcome,
         </h1>
         <h1 id="Name">
-            I'm Olivier <span id="LastName">Pezik Pajoow</span><span id="Slash">|</span>
+            I'm Olivier <span class="Animated">Pezik Pajoow</span><span class="Slash">|</span>
         </h1>
         <h1>
             A software developer <br>with a passion for <span id="Tag"> &lt;code&gt;</span>
@@ -64,10 +64,31 @@
     <img id="Border" src="Images/TestBorder2.png" alt="Border" >
     <section id="AboutSec">
         <section id="LeftSide">
-
+            <h1>
+                Hi! Here's something<br><span class=Animated>About me</span><span class="Slash">|</span>
+            </h1>
+            <h2>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid ipsa repellendus. Beatae culpa cumque eaque explicabo ipsam labore modi.
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ab adipisci amet architecto assumenda,
+                aut debitis deleniti doloremque,
+                eaque esse iusto nam perferendis porro rem saepe voluptatem!
+                A atque autem delectus deserunt dolorum et, iste porro quisquam quod sed. Ducimus fugiat officia perspiciatis qui?
+                Ad dolor fugit non perferendis tenetur voluptas. Atque blanditiis commodi cupiditate distinctio dolor eaque ex exercitationem,
+                impedit inventore ipsum iste laudantium magnam, molestias nulla quaerat quis sequi voluptatem.
+                Ad animi atque aut consequatur dolore doloremque dolorum impedit iure laborum modi nemo numquam odit officia, optio perspiciatis,
+                quam qui quia quibusdam, quidem quos reiciendis sunt vero voluptas voluptate.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dolores excepturi explicabo magni.
+                Commodi dicta eveniet fugiat iste reiciendis veritatis. A deleniti explicabo magni modi officiis pariatur quam quisquam sunt tempora vero!
+                Ducimus, exercitationem, obcaecati. Consectetur, explicabo fugiat. A aliquam architecto at,
+                consectetur cum cumque neque officiis provident rem soluta.
+            </p>
         </section>
         <section id="RightSide">
-
         </section>
     </section>
 </main>
