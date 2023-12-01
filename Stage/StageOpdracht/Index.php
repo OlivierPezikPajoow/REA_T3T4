889a557ee715bed1d9bcd21ad9f0b5655a0b9a14
Administrator <olivier.pezikpajoow@edu-kw1c.nl>
2023-12-01 16:01:24 +0100

    Stageopdracht

--- Stage/StageOpdracht/Index.php
+++ Stage/StageOpdracht/Index.php
@@ -103,7 +103,28 @@
             </section>
         </section>
         <section id="RightSide">
-
+            <h1>
+                What are my <br><span class=Animated> interests?</span><span class="Slash">|</span>
+            </h1>
+            <h2>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid ipsa repellendus.
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ab adipisci amet architecto assumenda,
+                aut debitis deleniti doloremque,
+                eaque esse iusto nam perferendis porro rem saepe voluptatem!
+                A atque autem delectus deserunt dolorum et, iste porro quisquam quod sed. Ducimus fugiat officia perspiciatis qui?
+                Ad dolor fugit non perferendis tenetur voluptas. Atque blanditiis commodi cupiditate distinctio dolor eaque ex exercitationem,
+                impedit inventore ipsum iste laudantium magnam, molestias nulla quaerat quis sequi voluptatem.
+                Ad animi atque aut consequatur dolore doloremque dolorum impedit iure laborum modi nemo numquam odit officia, optio perspiciatis,
+                quam qui quia quibusdam, quidem quos reiciendis sunt vero voluptas voluptate.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dolores excepturi explicabo magni.
+                Commodi dicta eveniet fugiat iste reiciendis veritatis. A deleniti explicabo magni modi officiis pariatur quam quisquam sunt tempora vero!
+                Ducimus, exercitationem, obcaecati. Consectetur, explicabo fugiat. A aliquam architecto at,
+                consectetur cum cumque neque officiis provident rem soluta.
+            </p>
         </section>
     </section>
 </main>
